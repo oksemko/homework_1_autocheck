2e5a1d46598ffd7fd0dbf58a469cb812f4b598f7
Oksana Semko <osemko@gmail.com>
2022-04-10 19:54:46 +0300

    solution_task2

---
+++ task2.js
@@ -0,0 +1,24 @@
+// Ім'я товару змінили на "Repair droid"
+// і збільшили його ціну на 1500 кредитів.
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+------------------------------------------------------------------------------
+    
+// Змінна pricePerItem оголошена за допомогою let
+// При оголошенні змінної pricePerItem присвоєно значення — число 2000
+// Змінній pricePerItem присвоєно нове значення, більше за попереднє на 1500
+// Змінна productName оголошена за допомогою let
+// При оголошенні змінної productName присвоєно значення — рядок "Droid"
+// Змінній productName присвоєно нове значення — рядок "Repair droid"
+    
+------------------------------------------------------------------------------
+
+let productName = "Droid";
+let pricePerItem = 2000;
+
+// Change code below this line
+productName = "Repair droid";
+pricePerItem = 2000 + 1500;
+
+console.log(productName);
+console.log(pricePerItem);
