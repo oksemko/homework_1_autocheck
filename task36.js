63620654c2d4b6083bcb2188df394d8ad7dbad19
Oksana Semko <osemko@gmail.com>
2022-04-10 19:46:10 +0300

    solve task1

---
+++ task36.js
@@ -0,0 +1,22 @@
+Оголошена функція checkForSpam(message).
+Виклик функції checkForSpam("Latest technology news") повертає false
+Виклик функції checkForSpam("JavaScript weekly newsletter")повертає false
+Виклик функції checkForSpam("Get best sale offers now!") повертає true
+Виклик функції checkForSpam("Amazing SalE, only tonight!") повертає true
+Виклик функції checkForSpam("Trust me, this is not a spam message") повертає true
+Виклик функції checkForSpam("Get rid of sPaM emails. Our book in on sale!") повертає true
+Виклик функції checkForSpam("[SPAM] How to earn fast money?") повертає true
+
+
+
+
+
+
+
+function checkForSpam(message) {
+  let result;
+  // Change code below this line
+
+  // Change code above this line
+  return result;
+}
