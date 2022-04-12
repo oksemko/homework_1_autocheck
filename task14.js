597a427c17397169f01da9f5834a21ede23999dc
Oksana Semko <osemko@gmail.com>
2022-04-13 01:37:42 +0300

    task14_solution

---
+++ task14.js
@@ -0,0 +1,28 @@
+// Функція isValidPassword(password) перевіряє рівність збереженого
+// і введеного паролів і повертає результат перевірки - буль true або false.
+// Змінна SAVED_PASSWORD зберігає значення попередньо збереженого пароля.
+// Введений пароль передається у параметр password.
+
+// Присвой змінній isMatch вираз перевірки рівності введених і попередньо
+// збережених паролів.Результатом виразу перевірки повинно бути true,
+// якщо значення збігаються, і false - якщо ні.
+
+---------------------------------------------------------------------------------
+
+// Оголошена функція isValidPassword(password)
+// У виразі перевірки паролів використаний оператор ===
+// Виклик isValidPassword("mangodab3st") повертає false
+// Виклик isValidPassword("kiwirul3z") повертає false
+// Виклик isValidPassword("jqueryismyjam") повертає true
+
+---------------------------------------------------------------------------------
+
+function isValidPassword(password) {
+  const SAVED_PASSWORD = 'jqueryismyjam';
+  // Change code below this line
+  
+  const isMatch = password === SAVED_PASSWORD;
+
+  // Change code above this line
+  return isMatch;
+}
